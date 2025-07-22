c402e68026df89da3c1e5ff6371c88638f1ce4ab
DopyGamer00 <sarrbaba1140@gmail.com>
2025-07-22 23:34:15 +0000

    Add files via upload

--- exam/user.h
+++ exam/user.h
@@ -1,20 +1,24 @@
 #ifndef USER_H
 #define USER_H
 
+
+
 typedef struct {
     char login[6];
     char password[20];
     char nom[30];
     char prenom[30];
     char telephone[15];
-    char role[10];          // "ADMIN" ou "PHAR"
-    int premier_login;      // 1 = changer mdp requis
+    char role[10];
+    int premier_login;
 } User;
+
 
 void ajouter_utilisateur();
 int connexion(User *u_connecte);
 void changer_password(User *u_connecte);
-void afficher_utilisateurs();
+void menu();
 
 #endif
+
 
